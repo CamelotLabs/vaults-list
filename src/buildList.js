e588ca06fd2f94253efebab738688e334fd2b16d
0xmyrddin <0xmyrddin@pm.me>
2023-10-21 22:59:29 +0200

    project: dynamic asset urls

--- src/buildList.js
+++ src/buildList.js
@@ -1,10 +1,16 @@
+require('dotenv').config()
 const defiedge = require("./vaults/defiedge.json");
 const gamma = require("./vaults/gamma.json");
 
+const BASE_URL = process.env.BASE_URL || "https://vaults-list.camelot.exchange"
+
 module.exports = function buildList() {
+    const vaults = [defiedge, gamma]
+    const processedVaults = JSON.parse(JSON.stringify(vaults).replace(/BASE_URL/g, BASE_URL))
+
     return {
         name: "Camelot Vaults",
         timestamp: new Date().toISOString(),
-        vaults: [defiedge, gamma]
+        vaults: processedVaults
     };
 };
