60f6d1f10ead5a1516f859462f3738549ac0cf83
0xmyrddin <0xmyrddin@pm.me>
2023-10-22 00:43:43 +0200

    tests: add unit tests

--- test/camelot-default.test.js
+++ test/camelot-default.test.js
@@ -7,45 +7,69 @@ const buildList = require('../src/buildList');
 
 const ajv = new Ajv({ allErrors: true, format: 'full' });
 const validator = ajv.compile(schema);
-let vaultslist;
+let vaultsData;
 
 before(async function () {
-  vaultslist = await buildList();
+  vaultsData = await buildList();
 });
 
 describe('buildList', () => {
-
   it('schema is valid', () => {
-    expect(validator(vaultslist)).to.equal(true);
+    expect(validator(vaultsData)).to.equal(true);
   });
 
+  it('contains no duplicate ids', () => {
+    const map = {};
+    for (let protocol of vaultsData.vaults) {
+      const key = protocol.name.toLowerCase();
+      expect(typeof map[key])
+          .to.equal('undefined', `duplicate name: ${protocol.name}`);
+      map[key] = true;
+    }
+  })
 
-  // TODO: WIP
-  // it('contains no duplicate ids', () => {
-  //   const map = {};
-  //   for (let protocol of vaultslist.protocols) {
-  //     const key = `${protocol.chainId}-${protocol.id.toLowerCase()}`;
-  //     expect(typeof map[key])
-  //         .to.equal('undefined', `duplicate name: ${protocol.id}`);
-  //     map[key] = true;
-  //   }
-  // })
-  //
-  // it('contains no duplicate names', () => {
-  //   const map = {};
-  //   for (let protocol of vaultslist.protocols) {
-  //     const key = `${protocol.chainId}-${protocol.name.toLowerCase()}`;
-  //     expect(typeof map[key])
-  //         .to.equal('undefined', `duplicate name: ${protocol.name}`);
-  //     map[key] = true;
-  //   }
-  // })
-  //
-  // it('all addresses are valid and checksummed', () => {
-  //   for (let protocol of vaultslist.protocols) {
-  //     for (let tokenAddress of protocol.tokens) {
-  //       expect(getAddress(tokenAddress)).to.eq(tokenAddress);
-  //     }
-  //   }
-  // });
+  it('contains no duplicate chains', () => {
+    const map = {};
+    for (let protocol of vaultsData.vaults) {
+      for (let chain of protocol.chains) {
+        const key = `${protocol.name.toLowerCase()}-${chain.chainId}`;
+        expect(typeof map[key])
+            .to.equal('undefined', `duplicate chainId for ${protocol.name.toLowerCase()}: ${chain.chainId}`);
+        map[key] = true;
+      }
+    }
+  })
+
+  it('contains no duplicate strategies', () => {
+    const map = {};
+    for (let protocol of vaultsData.vaults) {
+      for (let chain of protocol.chains) {
+        for (let strategy of chain.strategies) {
+          const key = strategy.address;
+          expect(typeof map[key])
+              .to.equal('undefined', `duplicate strategy: ${strategy.address}`);
+          map[key] = true;
+        }
+      }
+    }
+  })
+
+  it('depositProxy addresses are valid and checksummed', () => {
+    for (let protocol of vaultsData.vaults) {
+      for (let chain of protocol.chains) {
+        expect(getAddress(chain.depositProxy)).to.eq(chain.depositProxy);
+      }
+    }
+  });
+
+  it('strategy addresses are valid and checksummed', () => {
+    for (let protocol of vaultsData.vaults) {
+      for (let chain of protocol.chains) {
+        for (let strategy of chain.strategies) {
+          expect(getAddress(strategy.address)).to.eq(strategy.address);
+          expect(getAddress(strategy.poolAddress)).to.eq(strategy.poolAddress);
+        }
+      }
+    }
+  })
 });
