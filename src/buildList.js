83c39f29d354e283a852fb83b647d507165b085e
0xpercival <percival@camelot.fi>
2024-01-12 16:58:14 +0100

    vaults: steer: init

--- src/buildList.js
+++ src/buildList.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const defiedge = require("./vaults/defiedge.json");
 const gamma = require("./vaults/gamma.json");
 const jones = require("./vaults/jones.json");
+const steer = require("./vaults/steer.json");
 
 const BASE_URL = process.env.BASE_URL || "https://vaults-list.camelot.exchange"
 
 module.exports = function buildList() {
-    const vaults = [defiedge, gamma, jones]
+    const vaults = [defiedge, gamma, jones, steer]
     const processedVaults = JSON.parse(JSON.stringify(vaults).replace(/BASE_URL/g, BASE_URL))
 
     return {
