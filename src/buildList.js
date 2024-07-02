d1a14602a31cb57d3e77880f45b358c7ba3ffb2e
0xmyrddin <0xmyrddin@pm.me>
2024-07-02 04:28:52 +0200

    alm: add beefy

--- src/buildList.js
+++ src/buildList.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const beefy = require("./vaults/beefy.json");
 const defiedge = require("./vaults/defiedge.json");
 const gamma = require("./vaults/gamma.json");
 const jones = require("./vaults/jones.json");
@@ -7,7 +8,7 @@ const steer = require("./vaults/steer.json");
 const BASE_URL = process.env.BASE_URL || "https://vaults-list.camelot.exchange"
 
 module.exports = function buildList() {
-    const vaults = [defiedge, gamma, jones, steer]
+    const vaults = [beefy, defiedge, gamma, jones, steer]
     const processedVaults = JSON.parse(JSON.stringify(vaults).replace(/BASE_URL/g, BASE_URL))
 
     return {
